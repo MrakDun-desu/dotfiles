b7636ab519bd07815076a30babdc580d268981e9
MrakDundesu <m.danco@protonmail.com>
2025-10-18 18:47:45 +0300

    Change completion back to blink

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -200,7 +200,7 @@ require("lazy").setup({
             "mason-org/mason-lspconfig.nvim",
             "WhoIsSethDaniel/mason-tool-installer.nvim",
             { "j-hui/fidget.nvim", opts = {} },
-            "hrsh7th/cmp-nvim-lsp",
+            "saghen/blink.cmp",
         },
         config = function()
             vim.api.nvim_create_autocmd("LspAttach", {
@@ -268,6 +268,7 @@ require("lazy").setup({
                                 vim.lsp.protocol.Methods.textDocument_inlayHint,
                                 event.buf
                             )
+                            or client.name == "omnisharp"
                         )
                     then
                         map("<leader>ch", function()
@@ -306,12 +307,7 @@ require("lazy").setup({
                 },
             })
 
-            local capabilities = vim.lsp.protocol.make_client_capabilities()
-            capabilities = vim.tbl_deep_extend(
-                "force",
-                capabilities,
-                require("cmp_nvim_lsp").default_capabilities()
-            )
+            local capabilities = require("blink.cmp").get_lsp_capabilities()
 
             -- if I want to override individual server settings, do it here
             local servers = {
@@ -320,6 +316,21 @@ require("lazy").setup({
                         Lua = {
                             completion = {
                                 callSnippet = "Replace",
+                            },
+                        },
+                    },
+                },
+                emmet_ls = {
+                    filetypes = {
+                        "css",
+                        "html",
+                        "javascriptreact",
+                        "typescriptreact",
+                    },
+                    init_options = {
+                        html = {
+                            options = {
+                                ["bem_enabled"] = true,
                             },
                         },
                     },
@@ -336,32 +347,14 @@ require("lazy").setup({
                 handlers = {
                     function(server_name)
                         local server = servers[server_name] or {}
-                        if server_name == "emmet_ls" then
-                            require("lspconfig")[server_name].setup({
-                                capabilities = capabilities,
-                                filetypes = {
-                                    "css",
-                                    "html",
-                                    "javascriptreact",
-                                    "typescriptreact",
-                                },
-                                init_options = {
-                                    html = {
-                                        options = {
-                                            ["bem_enabled"] = true,
-                                        },
-                                    },
-                                },
-                            })
-                            return
-                        end
                         server.capabilities = vim.tbl_deep_extend(
                             "force",
                             {},
                             capabilities,
                             server.capabilities or {}
                         )
-                        require("lspconfig")[server_name].setup(server)
+                        vim.lsp.config(server_name, server)
+                        vim.lsp.enable({ server_name })
                     end,
                 },
             })
@@ -381,12 +374,69 @@ require("lazy").setup({
         },
         config = function()
             if vim.fn.executable("gleam") == 1 then
-                require("lspconfig").gleam.setup({})
+                vim.lsp.enable("gleam")
             end
             require("godotdev").setup({
                 csharp = false,
             })
         end,
+    },
+
+    {
+        "saghen/blink.cmp",
+        event = "VimEnter",
+        version = "1.*",
+        dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                version = "2.*",
+                build = (function()
+                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+                        return
+                    end
+                    return "make install_jsregexp"
+                end)(),
+                dependencies = {
+                    {
+                        "rafamadriz/friendly-snippets",
+                        config = function()
+                            require("luasnip.loaders.from_vscode").lazy_load()
+                        end,
+                    },
+                },
+                opts = {},
+            },
+            "folke/lazydev.nvim",
+        },
+        --- @module 'blink.cmp'
+        --- @type blink.cmp.Config
+        opts = {
+            keymap = {
+                preset = "default",
+            },
+
+            appearance = {
+                -- Adjusts spacing to ensure icons are aligned
+                nerd_font_variant = "normal",
+            },
+
+            completion = {
+                documentation = { auto_show = false, auto_show_delay_ms = 500 },
+            },
+
+            sources = {
+                default = { "lsp", "path", "snippets", "lazydev" },
+                providers = {
+                    lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
+                },
+            },
+
+            snippets = { preset = "luasnip" },
+
+            fuzzy = { implementation = "prefer_rust_with_warning" },
+
+            signature = { enabled = true },
+        },
     },
 
     {
@@ -419,133 +469,6 @@ require("lazy").setup({
                 typst = { "typstyle" },
             },
         },
-    },
-
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                build = (function()
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    {
-                        "rafamadriz/friendly-snippets",
-                        config = function()
-                            require("luasnip.loaders.from_vscode").lazy_load()
-                        end,
-                    },
-                },
-            },
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-nvim-lsp-signature-help",
-            "onsails/lspkind.nvim",
-        },
-        config = function()
-            -- See `:help cmp`
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
-            local lspkind = require("lspkind")
-            luasnip.config.setup({})
-
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                completion = { completeopt = "menu,menuone,noinsert" },
-
-                -- For an understanding of why these mappings were
-                -- chosen, you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
-                mapping = cmp.mapping.preset.insert({
-                    -- Select the [n]ext item
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    -- Select the [p]revious item
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-
-                    -- Scroll the documentation window [b]ack / [f]orward
-                    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-                    -- Accept ([y]es) the completion.
-                    --  This will auto-import if your LSP supports it.
-                    --  This will expand snippets if the LSP sent a snippet.
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-
-                    -- If you prefer more traditional completion keymaps,
-                    -- you can uncomment the following lines
-                    --['<CR>'] = cmp.mapping.confirm { select = true },
-                    --['<Tab>'] = cmp.mapping.select_next_item(),
-                    --['<S-Tab>'] = cmp.mapping.select_prev_item(),
-
-                    -- Manually trigger a completion from nvim-cmp.
-                    --  Generally you don't need this, because nvim-cmp will display
-                    --  completions whenever it has completion options available.
-                    ["<C-Space>"] = cmp.mapping.complete({}),
-
-                    -- Think of <c-l> as moving to the right of your snippet expansion.
-                    --  So if you have a snippet that's like:
-                    --  function $name($args)
-                    --    $body
-                    --  end
-                    --
-                    -- <c-l> will move you to the right of each of the expansion locations.
-                    -- <c-h> is similar, except moving you backwards.
-                    ["<C-l>"] = cmp.mapping(function()
-                        if luasnip.expand_or_locally_jumpable() then
-                            luasnip.expand_or_jump()
-                        end
-                    end, { "i", "s" }),
-                    ["<C-h>"] = cmp.mapping(function()
-                        if luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
-                        end
-                    end, { "i", "s" }),
-
-                    -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-                    --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-                }),
-                sources = {
-                    {
-                        name = "lazydev",
-                        -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-                        group_index = 0,
-                    },
-                    { name = "nvim_lsp" },
-                    { name = "luasnip" },
-                    { name = "path" },
-                    { name = "nvim_lsp_signature_help" },
-                },
-                formatting = {
-                    fields = { "kind", "abbr", "menu" },
-                    format = lspkind.cmp_format({
-                        mode = "symbol",
-                        maxwidth = 50,
-                        ellipsis_char = "…",
-                        before = function(entry, vim_item)
-                            vim_item.menu = ({
-                                nvim_lsp = "[LSP]",
-                                luasnip = "[Snippet]",
-                                buffer = "[Buffer]",
-                                path = "[Path]",
-                            })[entry.source.name]
-                            return vim_item
-                        end,
-                    }),
-                },
-            })
-        end,
     },
 
     {
