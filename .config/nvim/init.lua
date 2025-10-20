c225cc51ee6e19e37f6520f965c2b8253046a3aa
MrakDundesu <m.danco@protonmail.com>
2025-10-20 11:57:57 +0300

    Switch from luasnip to native snippets

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -389,37 +389,6 @@ require("lazy").setup({
         event = "VimEnter",
         version = "1.*",
         dependencies = {
-            {
-                "L3MON4D3/LuaSnip",
-                version = "2.*",
-                build = (function()
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    {
-                        "rafamadriz/friendly-snippets",
-                        config = function()
-                            require("luasnip.loaders.from_vscode").lazy_load()
-                        end,
-                    },
-                },
-                config = function()
-                    local luasnip = require("luasnip")
-                    vim.api.nvim_create_autocmd("ModeChanged", {
-                        pattern = "*:n",
-                        callback = function()
-                            if luasnip.in_snippet() then
-                                luasnip.unlink_current()
-                            end
-                        end,
-                        desc = "Clear snippet jump list when entering normal mode",
-                    })
-                    luasnip.setup({})
-                end,
-            },
             "folke/lazydev.nvim",
         },
         --- @module 'blink.cmp'
@@ -441,7 +410,7 @@ require("lazy").setup({
                     lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
                 },
             },
-            snippets = { preset = "luasnip" },
+            snippets = { preset = "default" },
             fuzzy = { implementation = "prefer_rust_with_warning" },
             signature = { enabled = true },
         },
